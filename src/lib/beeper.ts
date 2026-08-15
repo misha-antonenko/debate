60bc02b90978961615adc785c8ffbe308344eca2
Misha Antonenko <misha_antonenko@icloud.com>
2026-08-15 15:41:17 +0000

    Revert "Soften the timer end-of-time sound"
    This reverts commit 0da337467742215cc018e6f07e18f157071e577a.

--- src/lib/beeper.ts
+++ src/lib/beeper.ts
@@ -1,9 +1,5 @@
-const CHORD_HZ = [523.25, 659.25, 783.99] as const
-const MASTER_GAIN = 0.22
-const ATTACK_S = 0.04
-const RELEASE_S = 0.15
-const TREMOLO_HZ = 5
-const TREMOLO_DEPTH = 0.12
+const BEEP_FREQUENCY_HZ = 1000
+const BEEP_GAIN = 0.6
 
 const CHIME_HZ = [659.25, 783.99, 987.77, 1318.51] as const
 const CHIME_LEAD_S = 0.03
@@ -14,38 +10,21 @@ const SILENCE_GAIN = 0.0001
 
 export class Beeper {
   #context: AudioContext | null = null
-  #voices: OscillatorNode[] = []
-  #master: GainNode | null = null
+  #oscillator: OscillatorNode | null = null
 
   start(): void {
-    if (this.#voices.length) return
-    const context = (this.#context ??= new AudioContext())
-    void context.resume()
-    const now = context.currentTime
+    if (this.#oscillator) return
+    this.#context ??= new AudioContext()
+    void this.#context.resume()
 
-    const master = context.createGain()
-    master.gain.setValueAtTime(0, now)
-    master.gain.linearRampToValueAtTime(MASTER_GAIN, now + ATTACK_S)
-    master.connect(context.destination)
-
-    const tremolo = context.createOscillator()
-    const tremoloDepth = context.createGain()
-    tremolo.frequency.value = TREMOLO_HZ
-    tremoloDepth.gain.value = TREMOLO_DEPTH * MASTER_GAIN
-    tremolo.connect(tremoloDepth).connect(master.gain)
-    tremolo.start(now)
-
-    const voices = CHORD_HZ.map((frequencyHz) => {
-      const oscillator = context.createOscillator()
-      oscillator.type = "sine"
-      oscillator.frequency.value = frequencyHz
-      oscillator.connect(master)
-      oscillator.start(now)
-      return oscillator
-    })
-
-    this.#master = master
-    this.#voices = [...voices, tremolo]
+    const oscillator = this.#context.createOscillator()
+    const gain = this.#context.createGain()
+    oscillator.type = "square"
+    oscillator.frequency.value = BEEP_FREQUENCY_HZ
+    gain.gain.value = BEEP_GAIN
+    oscillator.connect(gain).connect(this.#context.destination)
+    oscillator.start()
+    this.#oscillator = oscillator
   }
 
   chime(): void {
@@ -73,13 +52,9 @@ export class Beeper {
   }
 
   stop(): void {
-    if (!this.#voices.length || !this.#context || !this.#master) return
-    const now = this.#context.currentTime
-    this.#master.gain.cancelScheduledValues(now)
-    this.#master.gain.setValueAtTime(this.#master.gain.value, now)
-    this.#master.gain.linearRampToValueAtTime(0, now + RELEASE_S)
-    for (const voice of this.#voices) voice.stop(now + RELEASE_S)
-    this.#voices = []
-    this.#master = null
+    if (!this.#oscillator) return
+    this.#oscillator.stop()
+    this.#oscillator.disconnect()
+    this.#oscillator = null
   }
 }
